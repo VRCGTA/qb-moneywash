485e22d7ce98ac0fce951ecea4ab0ddb4ec71abe
yukad2 <myuratq@gmail.com>
2024-02-06 00:36:42 +0900

    税率

--- config.lua
+++ config.lua
@@ -5,9 +5,9 @@
 -- IF YOU LEAVE MY DISCORD AFTER GETTING SUPPORT YOU MUST LIKE LITTLE BOYS 🐧
 Config = {
     Locations = {
-        { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 10 }, -- this is the location from the one on the fourms post!
+        { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 20 }, -- this is the location from the one on the fourms post!
         { name = "Location 2", x = 1234.56, y = -789.01, z = 32.10, tax = 15 },
-        { name = "Location 3", x = 987.65, y = -543.21, z = 12.34, tax = 20 },
+        { name = "Location 3", x = 987.65, y = -543.21, z = 12.34, tax = 10 },
     },
 }
 
