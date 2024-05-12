407514172cfa106e54e6bfff288821883aea5159
azutake <azusa_takemae@dwango.co.jp>
2024-05-13 03:28:52 +0900

    fix?

--- config.lua
+++ config.lua
@@ -6,8 +6,8 @@
 Config = {
     Locations = {
         { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 20 }, -- this is the location from the one on the fourms post!
-        { name = "Location 2", x = 1234.56, y = -789.01, z = 32.10, tax = 15 },
-        { name = "8176", x = 316.24, y = -1099.54, z = 29.40, tax = 10 },
+        { name = "PawnShop Ext", x = 403.98, y = 329.53, z = 114.94, tax = 15 },
+        { name = "8176", x = 316.24, y = -1099.54, z = 29.40, tax = 15 },
     },
 }
 
